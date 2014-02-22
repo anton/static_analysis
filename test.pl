1a1ee25da11809e2283e320247c4d0ac550896be
anton <haglundanton+github@gmail.com>
2014-02-22 19:28:54 +0100

    Fix regexp in unit test
    Signed-off-by: anton <haglundanton+github@gmail.com>

--- test.pl
+++ test.pl
@@ -2,18 +2,18 @@
 use strict;
 use warnings;
 use RRDs;
-use lib qw( Modules/ );
+use lib qw(Modules);
 use SourceFile;
 $|++;
 
 sub createProperRRDFilename {
     my $sourcefile = SourceFile->new(filepath => "foo.c");
     $sourcefile->generateRRDFilename();
-    $sourcefile->{'rrdfile'} =~ /foo_c.rrd/ || die "createProperRRDFilename fail";
+    $sourcefile->{'rrdfile'} =~ /^data\/foo_c.rrd$/ || die "createProperRRDFilename fail";
 
     $sourcefile = SourceFile->new(filepath => "/foo/bar/baz.c");
     $sourcefile->generateRRDFilename();
-    $sourcefile->{'rrdfile'} =~ /foo_bar_baz_c.rrd/ || die "createProperRRDFilename fail";
+    $sourcefile->{'rrdfile'} =~ /^data\/_foo_bar_baz_c.rrd$/ || die "createProperRRDFilename fail";
 }
 
 sub createRRDFile {
